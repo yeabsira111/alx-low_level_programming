c0db902a1e67011e6f59d698786e0c254264641f
yeabsira111 <yeshiyeabsira7@gmail.com>
2023-02-16 01:32:28 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stido.h>
+/**
+ * main - a program that prints the siz of various commputer types
+ * Return 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of a char:  %lu byts(s)\n*, (unsigned log)sizeof((a));
+printf("size of a int:  %lu byts(s)\n *, (unsigned)sizeof((b)):
+printf("size of a long int:  %lu byts(s)\n*, (unsigned log)sizeof((c));
+printf("size of a long long int: %lu byts(s)\n *, (unsigned)sizeof((d));
+printf("size of a float:  %lu byts(s)\n*, (unsigned log)sizeof((f));
+return (0);
+}
